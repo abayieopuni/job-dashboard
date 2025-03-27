ce17a72e63d1f8ed438ec374d7f2b6d8fef3ddb3
Abayie Opuni <abayieremi@gmail.com>
2025-03-27 02:46:27 -0400

    commitiing stats pages

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -28,49 +28,76 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart',
+      text: ' Bar Chart',
     },
   },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const weeklyLabels = ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
+const monthlyLabels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+];
 
-export const data = {
-  labels,
+export const weeklyData = {
+  labels: weeklyLabels,
   datasets: [
     {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.number.int({ min: 0, max: 100 })),
+      label: 'Weekly',
+      data: weeklyLabels.map(() => faker.number.int({ min: 0, max: 100 })),
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
+  ],
+};
+
+export const monthlyData = {
+  labels: monthlyLabels,
+  datasets: [
     {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.number.int({ min: 0, max: 100 })),
+      label: 'Monthly',
+      data: monthlyLabels.map(() => faker.number.int({ min: 0, max: 1000 })),
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
   ],
 };
 
 const Stats = () => {
+  const [isWeekly, setIsWeekly] = useState(true);
+
   return (
     <>
-      <section>
-        <h1>Your Website Statistics</h1>
-        <p>
+      <section className="border-2 shadow-lg m-6 p-6">
+        <h1 className='font-medium '>Your Website Statistics</h1>
+        <p className=" text-lg leading-[1.5]">
           Learn more about the activity and the behavior of the site visitors.
         </p>
         <br />
-        <ul>
+        <ul className="flex justify-center gap-3 ">
           <li>
-            <button>Button 1</button>
+            <button
+              onClick={() => setIsWeekly(true)}
+              className={`relative bg-transparent text-lg border-2 outline-0 rounded-md p-1 ${isWeekly ? 'bg-blue-500 ' : 'text-blue-500'}`}
+            >
+              Weekly Data
+            </button>
           </li>
           <li>
-            <button>Button 2</button>
+            <button
+              onClick={() => setIsWeekly(false)}
+              className={`relative bg-transparent text-lg border-2 outline-0 rounded-md p-1 ${!isWeekly ? 'bg-blue-500' : 'text-blue-500'}`}
+            >
+              Monthly Data
+            </button>
           </li>
         </ul>
 
         {/* Bar Chart */}
-        <Bar data={data} options={options} />
+        <Bar data={isWeekly ? weeklyData : monthlyData} options={options} />
       </section>
     </>
   );
